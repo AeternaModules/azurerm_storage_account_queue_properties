45112facb59462088ef49a6531dcd891bb13593f
Daniel Allen <dan@aeternagroup.net>
2026-01-11 19:28:24 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "storage_account_queue_propertiess" {
+  description = "All storage_account_queue_properties resources"
+  value       = azurerm_storage_account_queue_properties.storage_account_queue_propertiess
+}
+
