aed3cafb3847f9b7519e8ec77f5a1333721586ad
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:45 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "storage_account_queue_propertieses" {
-  description = "All storage_account_queue_properties resources"
-  value       = azurerm_storage_account_queue_properties.storage_account_queue_propertieses
-}
 output "storage_account_queue_propertieses_cors_rule" {
-  description = "List of cors_rule values across all storage_account_queue_propertieses"
-  value       = [for k, v in azurerm_storage_account_queue_properties.storage_account_queue_propertieses : v.cors_rule]
+  description = "Map of cors_rule values across all storage_account_queue_propertieses, keyed the same as var.storage_account_queue_propertieses"
+  value       = { for k, v in azurerm_storage_account_queue_properties.storage_account_queue_propertieses : k => v.cors_rule }
 }
 output "storage_account_queue_propertieses_hour_metrics" {
-  description = "List of hour_metrics values across all storage_account_queue_propertieses"
-  value       = [for k, v in azurerm_storage_account_queue_properties.storage_account_queue_propertieses : v.hour_metrics]
+  description = "Map of hour_metrics values across all storage_account_queue_propertieses, keyed the same as var.storage_account_queue_propertieses"
+  value       = { for k, v in azurerm_storage_account_queue_properties.storage_account_queue_propertieses : k => v.hour_metrics }
 }
 output "storage_account_queue_propertieses_logging" {
-  description = "List of logging values across all storage_account_queue_propertieses"
-  value       = [for k, v in azurerm_storage_account_queue_properties.storage_account_queue_propertieses : v.logging]
+  description = "Map of logging values across all storage_account_queue_propertieses, keyed the same as var.storage_account_queue_propertieses"
+  value       = { for k, v in azurerm_storage_account_queue_properties.storage_account_queue_propertieses : k => v.logging }
 }
 output "storage_account_queue_propertieses_minute_metrics" {
-  description = "List of minute_metrics values across all storage_account_queue_propertieses"
-  value       = [for k, v in azurerm_storage_account_queue_properties.storage_account_queue_propertieses : v.minute_metrics]
+  description = "Map of minute_metrics values across all storage_account_queue_propertieses, keyed the same as var.storage_account_queue_propertieses"
+  value       = { for k, v in azurerm_storage_account_queue_properties.storage_account_queue_propertieses : k => v.minute_metrics }
 }
 output "storage_account_queue_propertieses_storage_account_id" {
-  description = "List of storage_account_id values across all storage_account_queue_propertieses"
-  value       = [for k, v in azurerm_storage_account_queue_properties.storage_account_queue_propertieses : v.storage_account_id]
+  description = "Map of storage_account_id values across all storage_account_queue_propertieses, keyed the same as var.storage_account_queue_propertieses"
+  value       = { for k, v in azurerm_storage_account_queue_properties.storage_account_queue_propertieses : k => v.storage_account_id }
 }
 
